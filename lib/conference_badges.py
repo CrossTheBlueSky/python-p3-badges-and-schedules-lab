12e4fcafd78946fbd90938b13bdeabb316e2b9ba
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-11 15:53:35 -0700

    passeD

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+
+    room = 1
+    out = []
+    for name in names:
+        out.append(f"Hello, {name}! You'll be assigned to room {room}!")
+        room += 1
+    return out
 
 def printer(names):
-    return None
+    for name in names:
+        print(badge_maker(name))
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
